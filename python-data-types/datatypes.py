979f198f901036727881b3abf5aea637d6706d4e
Jonas Willemsen <94461380+jonaswillemsen@users.noreply.github.com>
2025-01-14 16:49:38 +0000

    Create datatypes.py

---
+++ python-data-types/datatypes.py
@@ -0,0 +1,77 @@
+# Type assignment
+a = str("123")
+b = float(2)
+c = int(1.9) #just throws away the .9; not rounded!
+print ('a is a ' + str(type(a))+' with the value ' + str(a))
+print ('b is a ' + str(type(b))+' with the value ' + str(b))
+print ('c is a ' + str(type(c))+' with the value ' + str(c))
+
+#
+# Numeric types
+# 
+
+a = 10  #int
+b = 3   #int
+c = 2.5 #float
+d = -2  #int
+
+print(a+b) #output: 13, an int
+print(a+c) #output: 12.5, a float
+
+print(float(a)) #output: 10.0
+print(int(2.88)) #output: 2; just throws away the decimal part
+
+print(round(2.88)) #output: 3
+print(round(2.88,1)) #output: 2.9
+
+#
+# Strings
+#
+
+str_a = "Hello" #string
+str_b = "SCW!"  #string
+
+str_ab = str_a + " " + str_b #python repurposes the "+" to mean string concatenation as well as addition
+print(str_ab)                #output: Hello SCW!
+
+str_repeated = str_ab * 3
+print(str_repeated)          #output: Hello SCW!Hello SCW!Hello SCW!
+
+print(len(str_a))  			 #output: 5
+print(str_a[0])				 #output: H
+print(str_a[0:3])			 #output: Hel (give me 3 characters starting at 0)
+print(str_a[3:])			 #output: lo (give me everything starting at 3)
+
+#
+# Lists
+#
+
+fruits = ["banana", "lychee", "raspberry", "apple"]
+print(fruits[0]) 		#output: banana (string)
+print(fruits[0:2])		#output: ['banana','lychee'] (list!)
+print(fruits[-1])		#output: apple (string)
+
+fruits.append("orange")
+print(fruits) 			#output: ['banana', 'lychee', 'raspberry', 'apple', 'orange']
+
+print("orange" in fruits) #output: True
+print("tomato" in fruits) #output: False
+
+fruits.sort()
+print(fruits)			#output: ['apple', 'banana', 'lychee', 'orange', 'raspberry']
+
+mixed_list = ["blue", "green", False, 2, 2.55] #each item in a list can be a different data type!
+for item in mixed_list:
+	print(type(item)) 	#output:<class 'str'> <class 'str'> <class 'bool'> <class 'int'> <class 'float'>
+
+str_number = "123"
+print(type(str_number)) #output:<class 'str'>
+str_number=int(str_number)
+print(type(str_number)) #output:<class 'int'>
+
+a = 321
+print(type(a)) 			#output:<class 'int'>
+a = float(a)
+print(type(a)) 			#output:<class 'float'>
+
+#b = int("kiwi")			#output: ValueError: invalid literal for int() with base 10: 'kiwi'
